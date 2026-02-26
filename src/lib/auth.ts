d04573994228df052b295b60ef0a6e2a862eca9a
asifalam515 <asibulalam515@gmail.com>
2026-02-26 15:01:51 +0600

    better auth base url updated

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,7 +2,7 @@ import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: "https://skill-bridge-server-ashen.vercel.app",
+  baseURL: "https://skill-bridge-4216-server.vercel.app",
 
   fetchOptions: {
     credentials: "include",
